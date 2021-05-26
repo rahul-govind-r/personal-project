4418d5305b5da915bfa7f4ef700a8e533529f908
rahul-govind-r <rahulgovindrajeevan@gmail.com>
2021-05-26 18:34:46 +0530

    feature

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const cowinApi = async () => {
     var date = d.getDate() + '-' + (d.getMonth() + 1) + '-' + d.getFullYear()
     const response = await fetch('https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=304&date=' + date)
     const myJson =  await response.json(); //extract JSON from the http response
+    var theDiv = document.getElementById("root");
+    var content = document.createTextNode("<div>" + response + "</div>");
+    theDiv.appendChild(content);
     return myJson.centers
 }
 
@@ -21,9 +24,9 @@ const checkAvailability = async (cowinData) => {
         for ( j=0; j < sessions.length; j++ )
         {
             session=sessions[j]
-            if (session.min_age_limit === 18 && session.available_capacity_dose1 !== 0)
+            if (session.min_age_limit === 18 && session.available_capacity_dose1 === 0)
             {
-                message = "Hi, Vaccine slot available at " + cowinData[i].name + " on " + session.date + ". The address of the location is " + cowinData[i].address
+                message = "Hi, " + session.vaccine + " is available at " + cowinData[i].name + " on " + session.date + ". The address of the location is " + cowinData[i].address
                 url = 'https://api.telegram.org/bot1886526038:AAH2eZ8EgSeBcyvwYBKYDzq_Jhryo48ZSe0/sendMessage?chat_id=@slotAlertlocalktm&text=' + encodeURIComponent(message)
                 await sendtoTelegram()
             }
